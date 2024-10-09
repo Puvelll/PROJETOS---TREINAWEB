73425ebcf7fc79f61324dbb7a12fd4ad2ba9f535
Tomé Nery <98493848+Puvelll@users.noreply.github.com>
2024-10-09 15:03:07 -0300

    09/10

---
+++ C# - FUNDAMENTOS/CSHARP -FUNDAMENTOS/CSHARP -FUNDAMENTOS/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Digite a sua idade");
+var idadeInput = Console.ReadLine();
+var idade = Convert.ToInt32(idadeInput);
+
+var teste = idade >= 18 ? "true" :  "false";
+
+Console.WriteLine(teste);
---
+++ C# - FUNDAMENTOS/FUNDAMENTOS - 01/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+int idade;
+idade = 21;
+
+var nome;
